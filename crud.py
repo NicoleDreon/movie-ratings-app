3c92cee5bc4267840903b7579a990a86ec0e8b87
Nicole Dreon <dreon.nicole@gmail.com>
2021-03-25 19:49:07 +0000

    add create_movie and create_rating functions

--- crud.py
+++ crud.py
@@ -1,17 +1,40 @@
 """CRUD operations."""
 
-from model import db, User, Movie, Ratings, connect_to_db
+from model import db, User, Movie, Rating, connect_to_db
 
 
 def create_user(email, password):
     """Create and return a new user."""
 
-    user = User(email=self.email, password=self.password)
+    user = User(email=email, password=password)
 
     db.session.add(user)
     db.session.commit()
 
     return user
+
+
+def create_movie(title, overview, relese_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview,
+                  relese_date=relese_date, poster_path=poster_path)
+
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
+
+
+def create_rating(user, movie, score):
+    """Create and return a new rating"""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    db.session.add(rating)
+    db.session.commit()
+
+    return rating
 
 
 if __name__ == '__main__':
